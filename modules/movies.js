d0eabec7281d186a6e1320a3ab2562e6390a4a40
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-04-21 11:53:06 -0700

    lab10 bopped

--- modules/movies.js
+++ modules/movies.js
@@ -2,17 +2,27 @@
 
 const axios = require('axios');
 
+let cache = {};
+
 async function getMovie(request, response, next){
   try {
     let city = request.query.city;
+    let key = `city-${city}`;
 
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
+    if(cache[key] && (Date.now() - cache[key].timeStamp) < 8.64e+7) {
+      response.status(200).send(cache[key].data);
+    } else {
+      let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
+      let movie = await axios.get(url);
+      let dataToSend = movie.data.results.map(obj => new Movie(obj));
 
-    let movie = await axios.get(url);
+      cache[key] = {
+        data: dataToSend,
+        timeStamp: Date.now(),
+      };
 
-    let dataToSend = movie.data.results.map(obj => new Movie(obj));
-
-    response.status(200).send(dataToSend);
+      response.status(200).send(dataToSend);
+    }
   } catch (error) {
     next(error);
   }
--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-// const axios = require('axios');
 const getWeather = require('./modules/weather');
 const getMovie = require('./modules/movies');
 
